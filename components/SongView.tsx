aaba81215bb4592ddf0c8f6f16470937465c403c
Alex <alex@mbp-alex.lan>
2026-09-09 20:41:20 +0300

    F diez

--- components/SongView.tsx
+++ components/SongView.tsx
@@ -129,7 +129,7 @@ export default function SongView({
 
   const transposeChord = (chord: string, delta: number, capoOffset: number): string => {
     return chord.replace(/([A-G][b#]?)/g, (match) => {
-      let note = match.replace("A#", "Bb").replace("C#", "Db").replace("D#", "Eb").replace("F#", "Gb").replace("G#", "Ab");
+      let note = match.replace("A#", "Bb").replace("C#", "Db").replace("D#", "Eb").replace("G#", "Ab");
       const index = NOTES.indexOf(note);
       if (index === -1) return match;
       const finalDelta = delta - capoOffset;
